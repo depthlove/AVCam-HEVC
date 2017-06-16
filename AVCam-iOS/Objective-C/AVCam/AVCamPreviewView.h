a41a57c8407d35787e1e546ec31da37b5127ef87
depthlove <suntongmian@163.com>
2017-06-16 15:40:44 +0800

    support H.265（HEVC）to encode video or photo

---
+++ AVCam-iOS/Objective-C/AVCam/AVCamPreviewView.h
@@ -0,0 +1,18 @@
+/*
+See LICENSE.txt for this sample’s licensing information.
+
+Abstract:
+Application preview view.
+*/
+
+@import UIKit;
+
+@class AVCaptureSession;
+
+@interface AVCamPreviewView : UIView
+
+@property (nonatomic, readonly) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+@property (nonatomic) AVCaptureSession *session;
+
+@end
